5d67c637a25a0feaa7a3a08bc85e25ff917e66c2
Ryan Lane <ryan@ryanalane.com>
2015-04-17 14:30:04 -0700

    Added filename as first comment

--- extract.pl
+++ extract.pl
@@ -1,4 +1,5 @@
-% feature extraction predicates
+%%% 'extract.pl'
+%%% feature extraction predicates
 extract([], Feature:Value) :- !,fail.
 extract([Feature:Value|_], Feature:Value).
 extract([Feature1:Value1|Rest], Feature2:Value2) :- extract(Rest, Feature2:Value2).
